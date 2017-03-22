fa0cf9ce79d3efed342fc50e786031ff6b21d392
Torey Hickman <torey@toreyhickman.com>
2017-03-22 10:16:50 -0400

    Ammends e-mail introduction

--- app/models/email/email.rb
+++ app/models/email/email.rb
@@ -1,3 +1,3 @@
 module Email
-  Introduction = "Over the past three weeks, the following pull requests were merged to the unified phase guides."
+  Introduction = "In preparation for the upcoming phase, Curriculum would like to alert you to changes in the unified phase guides.  Over the past three weeks, the following pull requests were merged to the guides."
 end
